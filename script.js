ff4502bba5e5d75e668c6d50c7a118037af25472
Vlad Zobnin <vladopodobniy@gmail.com>
2020-09-22 22:57:56 +0300

    fix bug

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ const first = new Unit('Sniper', 50, 100);
 const second = new Unit('Infantryman', 100, 30);
 const third = new Unit('Sapper', 70, 80);
 
+// console.log(first);
+// first.showUnit();
+// second.showUnit();
+// third.showUnit();
 
-first.showUnit();
-second.showUnit();
-third.showUnit();
+// console.log(first.clone());
+// second.clone();
 
-const units = [first, second, third];
+const units = [first, second, third, four = second.clone()];
 
 const team = new Squad(units);
 
